44e4ac490d6c8a3548eec27b04b4d31c71836280
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2024-06-17 15:56:32 +0800

    public

--- public/js/main.js
+++
@@ -1,8 +0,0 @@
-
-if (window.location.pathname == "/" || window.location.pathname == "/index.html"){
-    document.getElementsByTagName("title")[0].innerText = 'HotPE - 一个纯净、强大、优雅的PE工具箱';
-}
-
-if (window.location.host.substring(0,window.location.host.indexOf(".")) != "www" && window.location.host.substring(0,window.location.host.indexOf(".")) != ""  && window.location.host.substring(0,window.location.host.indexOf(".")) != "bak"){
-    window.open ( 'https://www.hotpe.top' + window.location.pathname, '_self');
-}
